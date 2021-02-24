ec4b10237d9afe61511b28707e97f59d7b3a88f2
bujosa <davidbujosa@gmail.com>
2021-02-24 08:19:35 -0400

    Fixed signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { Response, Request } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.post("/api/users/signup", (res: Response, req: Request) => {
   const { email, password } = req.body;
-  res.send("Hi there");
+  if (!email || typeof email !== "string") {
+    res.status(400).send("Provide a valid email");
+  }
 });
 
 export { router as signupRouter };
