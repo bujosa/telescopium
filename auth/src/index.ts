5c9f8a4f533b1fb6c4c29935ec333dc175ced9e7
bujosa <davidbujosa@gmail.com>
2021-02-26 00:14:54 -0400

    Added mongo to the project

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -22,6 +24,19 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-Service:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
+
+start();
