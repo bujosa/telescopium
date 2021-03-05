adee8305611a2d11dab5bdd12f8196739c2ccd8b
bujosa <davidbujosa@gmail.com>
2021-03-05 01:45:14 -0400

    Added jsonwebtoken

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
-
+import cookieSession from "cookie-session";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -11,7 +11,14 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
