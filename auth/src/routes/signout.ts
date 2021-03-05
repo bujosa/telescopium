7025b139c9a62fe648f152a28d8977426ef23272
bujosa <davidbujosa@gmail.com>
2021-03-05 02:58:59 -0400

    Added current user middleware

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express, { Response, Request } from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req: Request, res: Response) => {
-  res.send("Hi there");
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
