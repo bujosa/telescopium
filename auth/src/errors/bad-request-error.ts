176fe410f526a5bac27d8ed5d1b873b7af403b8c
bujosa <davidbujosa@gmail.com>
2021-03-04 20:12:27 -0400

    Fixed bad request error

--- auth/src/errors/bad-request-error.ts
+++ auth/src/errors/bad-request-error.ts
@@ -3,7 +3,12 @@ import { CustomError } from "./custom-error";
 export class BadRequestError extends CustomError {
   statusCode = 400;
 
+  constructor(public message: string) {
+    super(message);
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+
   serializeErrors() {
-    return [{ message: "" }];
+    return [{ message: this.message }];
   }
 }
