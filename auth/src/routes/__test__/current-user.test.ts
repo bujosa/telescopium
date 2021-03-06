01194d11b3a19bc52b1938ef901d1c8dfe9a62de
bujosa <davidbujosa@gmail.com>
2021-03-06 19:15:49 -0400

    Added last test on current user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,18 +2,22 @@ import request from "supertest";
 import { app } from "../../app";
 
 it(`responds with details about the current user`, async () => {
-  await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
+  const cookie = await global.signin();
 
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it(`responds with null if not authenticated`, async () => {
   const response = await request(app)
     .get("/api/users/currentuser")
     .send()
     .expect(200);
 
-  console.log(response.body);
+  expect(response.body.currentUser).toEqual(null);
 });
