9ae96c6631371a9fea8b195513a047aa5dcecb97
bujosa <davidbujosa@gmail.com>
2021-03-07 00:59:40 -0400

    Fixed yaml files

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       const response = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
       return response.data;
     } catch (err) {
       setErrors(
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,24 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      "http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
