a5c6611683aebf14edf6cf5bd9fa47d1a30351bc
bujosa <davidbujosa@gmail.com>
2021-03-10 02:37:28 -0400

    Fixed packages json

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,10 +2,6 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
 import { errorHandler, NotFoundError } from "@ticketing-bujosa/common";
 import * as dotenv from "dotenv";
 
@@ -21,11 +17,6 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
