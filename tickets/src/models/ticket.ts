5fa95c060747df718383a35d0f85ba3c9712fb77
bujosa <davidbujosa@gmail.com>
2021-03-10 03:40:12 -0400

    Fixed ticket

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,1 +1,39 @@
 import mongoose from "mongoose";
+import {
+  ITicket,
+  ITicketDoc,
+  ITicketModel,
+} from "../interfaces/ticket.interface";
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (ticket: ITicket) => {
+  return new Ticket(ticket);
+};
+
+const Ticket = mongoose.model<ITicketDoc, ITicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
