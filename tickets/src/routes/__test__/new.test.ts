105f0b55befdc131d1349fb816375f643bef8612
bujosa <davidbujosa@gmail.com>
2021-03-10 02:57:48 -0400

    Fixed new test

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+
+it("Has a route handler listening to /api/tickets fro post request", async () => {});
+
+it("Can only be accessed if the user is signed in", async () => {});
+
+it("return an error if an invalid title is provided", async () => {});
+
+it("return an error if an invalid price is provided", async () => {});
+
+it("creates a tickets with valid inputs", async () => {});
