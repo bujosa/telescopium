08dcb7d522208da2dd30c4a46805dd179ea5e698
bujosa <davidbujosa@gmail.com>
2021-03-10 03:35:23 -0400

    Added ticket and ticket interface

---
+++ tickets/src/interfaces/ticket.interface.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export interface ITicket {
+  title: string;
+  price: number;
+  user: string;
+}
+
+export interface ITicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  user: string;
+}
+
+export interface ITicketModel extends mongoose.Model<ITicketDoc> {
+  build(ticket: ITicket): ITicketDoc;
+}
