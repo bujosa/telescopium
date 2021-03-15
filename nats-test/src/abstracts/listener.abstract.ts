85f1b681a083eb9babf07c076f4b84c573a5aeb4
bujosa <davidbujosa@gmail.com>
2021-03-15 04:01:11 -0400

    Added listener

---
+++ nats-test/src/abstracts/listener.abstract.ts
@@ -0,0 +1,21 @@
+import { Stan } from "node-nats-streaming";
+
+export abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+}
