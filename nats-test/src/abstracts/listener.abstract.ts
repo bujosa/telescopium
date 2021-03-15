c3735bc11d7538a1a2d18ff7f09785eb569f7af9
bujosa <davidbujosa@gmail.com>
2021-03-15 04:06:05 -0400

    Finished abstract listener

--- nats-test/src/abstracts/listener.abstract.ts
+++ nats-test/src/abstracts/listener.abstract.ts
@@ -1,8 +1,9 @@
-import { Stan } from "node-nats-streaming";
+import { Message, Stan } from "node-nats-streaming";
 
 export abstract class Listener {
   abstract subject: string;
   abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
@@ -17,5 +18,27 @@ export abstract class Listener {
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
       .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
   }
 }
