ae55c6a7a5bd83d59cca24e901d6f9167328fa09
bujosa <davidbujosa@gmail.com>
2021-03-17 03:48:29 -0400

    Fixed index

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,14 @@ import { natsWraper } from "./nats-wrapper";
 const start = async () => {
   try {
     await natsWraper.connect("ticketing", "dfrfrf", "http://nats-service:4222");
+    natsWraper.client.on("close", () => {
+      console.log("Nats Connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWraper.client.close());
+    process.on("SIGTERM", () => natsWraper.client.close());
+
     await mongoose.connect(process.env.MONGO_URL!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -14,14 +14,6 @@ class NatsWraper {
   connect(cluster: string, client: string, url: string): Promise<void> {
     this._client = nats.connect(cluster, client, { url });
 
-    this._client.on("close", () => {
-      console.log("Nats Connection closed");
-      process.exit();
-    });
-
-    process.on("SIGINT", () => this.client.close());
-    process.on("SIGTERM", () => this.client.close());
-
     return new Promise((resolve, reject) => {
       this.client.on("connect", () => {
         console.log("Connected to NATS");
