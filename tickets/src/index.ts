4fbcf6cfe9c25aa055f82ab2054af641c1d1c6bf
bujosa <davidbujosa@gmail.com>
2021-03-17 05:33:08 -0400

    Eliminated bad code

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,11 +4,18 @@ import { natsWraper } from "./nats-wrapper";
 import { randomBytes } from "crypto";
 
 const start = async () => {
+  if (!process.env.JWT) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
   try {
     await natsWraper.connect(
       process.env.NAST_CLUSTER_ID!,
       randomBytes(4).toString("hex"),
-      process.env.NAST_URL!
+      process.env.NATS_URL!
     );
     natsWraper.client.on("close", () => {
       console.log("Nats Connection closed");
