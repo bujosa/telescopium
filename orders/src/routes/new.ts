ecb3ef60808fb4cf1e81a84fa953ba08d0cbc913
bujosa <davidbujosa@gmail.com>
2021-03-18 03:05:29 -0400

    Added validation on new

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,17 @@
+import { requireAuth, validateRequest } from "@ticketing-bujosa/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [body("ticketId").not().isEmpty().withMessage("TicketId must be provided")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
