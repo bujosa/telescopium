1faa13b6db37aa0de4550adbd88e25a0d61fb97e
bujosa <davidbujosa@gmail.com>
2021-03-24 05:38:21 -0400

    Updated index ts

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,51 +1,15 @@
-import request from "supertest";
-import { app } from "../app";
-import { Ticket } from "../models/ticket";
+import { requireAuth } from "@ticketing-bujosa/common";
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
-const buildTicket = async () => {
-  const ticket = Ticket.build({
-    title: "concert",
-    price: 20,
-  });
-  await ticket.save();
+const router = express.Router();
 
-  return ticket;
-};
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    user: req.currentUser!.id,
+  }).populate("ticket");
 
-it("fetches orders for an particular user", async () => {
-  const ticketOne = await buildTicket();
-  const ticketTwo = await buildTicket();
-  const ticketThree = await buildTicket();
-
-  const userOne = global.signin();
-  const userTwo = global.signin();
-
-  await request(app)
-    .post("/api/orders")
-    .set("Cookie", userOne)
-    .send({ ticket: ticketOne.id })
-    .expect(201);
-
-  const { body: orderOne } = await request(app)
-    .post("/api/orders")
-    .set("Cookie", userTwo)
-    .send({ ticket: ticketTwo.id })
-    .expect(201);
-
-  const { body: orderTwo } = await request(app)
-    .post("/api/orders")
-    .set("Cookie", userTwo)
-    .send({ ticket: ticketThree.id })
-    .expect(201);
-
-  const response = await request(app)
-    .get("/api/orders")
-    .set("Cookie", userTwo)
-    .expect(200);
-
-  expect(response.body.length).toEqual(2);
-  expect(response.body[0].id).toEqual(orderOne.id);
-  expect(response.body[1].id).toEqual(orderTwo.id);
-  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
-  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+  res.send(orders);
 });
+
+export { router as indexOrderRouter };
