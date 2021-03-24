9c91960ff1eb17142175bde074adf4b2c1436983
bujosa <davidbujosa@gmail.com>
2021-03-24 05:32:55 -0400

    Added index test

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it("fetches orders for an particular user", async () => {
+  // Create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticket: ticketOne.id })
+    .expect(201);
+
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticket: ticketTwo.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticket: ticketThree.id })
+    .expect(201);
+
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
