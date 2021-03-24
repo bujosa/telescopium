23045e8a8168e8e155ef83ad2221c6a48c27b52f
bujosa <davidbujosa@gmail.com>
2021-03-24 05:58:13 -0400

    Added ticket created listener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,13 @@
+import {
+  Listener,
+  Subjects,
+  TicketCreatedEvent,
+} from "@ticketing-bujosa/common";
+import { Message } from "node-nats-streaming";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = "orders-service";
+
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+}
