82761c9b65f7be1663489f751d7dda64497e1821
bujosa <davidbujosa@gmail.com>
2021-03-25 02:37:21 -0400

    Finished ticket updated listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,3 +1,27 @@
+import {
+  Listener,
+  Subjects,
+  TicketUpdateEvent,
+} from "@ticketing-bujosa/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
-export class TicketUpdatedListener {}
+export class TicketUpdatedListener extends Listener<TicketUpdateEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdateEvent["data"], msg: Message) {
+    const { title, price, id } = data;
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
