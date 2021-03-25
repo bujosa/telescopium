f641eaf5cf93205aed67e5211f74b45c30796228
bujosa <davidbujosa@gmail.com>
2021-03-25 03:24:17 -0400

    Finished ticket test

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,6 +1,6 @@
 import { Ticket } from "../ticket";
 
-it("implements optimistic concurrency control", async () => {
+it("implements optimistic concurrency control", async (done) => {
   const ticket = Ticket.build({
     title: "testing",
     price: 200,
@@ -8,4 +8,40 @@ it("implements optimistic concurrency control", async () => {
   });
 
   await ticket.save();
+
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  firstInstance?.set({ price: 10 });
+  secondInstance?.set({ price: 15 });
+
+  await firstInstance!.save();
+
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return done();
+  }
+
+  throw new Error("Should not reach this point");
+});
+
+it("inscrements the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "testing",
+    price: 200,
+    user: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(3);
 });
