01d6e6869fa9b934a123114f1c324b9db225c935
bujosa <davidbujosa@gmail.com>
2021-03-25 02:40:54 -0400

    Fixed comment and integrated listeners on index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
@@ -7,7 +9,7 @@ const start = async () => {
     throw new Error("JWT_KEY must be defined");
   }
   if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URI must be defined");
+    throw new Error("MONGO_URL must be defined");
   }
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
@@ -18,6 +20,7 @@ const start = async () => {
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error("NATS_CLUSTER_ID must be defined");
   }
+
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
@@ -33,11 +36,15 @@ const start = async () => {
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
 
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
+
     console.log("Mongo Connection is open");
   } catch (error) {
     console.log("error");
