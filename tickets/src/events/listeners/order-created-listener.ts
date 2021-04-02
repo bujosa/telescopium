9b730f1aefe3391b70176f4711caeb807c156ee6
bujosa <davidbujosa@gmail.com>
2021-04-02 19:08:19 -0400

    Finished created listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -4,11 +4,24 @@ import {
   Subjects,
 } from "@ticketing-bujosa/common";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ order: data.id });
+
+    await ticket.save();
+
+    msg.ack();
+  }
 }
