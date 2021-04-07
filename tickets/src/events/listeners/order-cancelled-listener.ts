ee5b8f38abfe54baa3fc3c1fc8ea9c48a58c158c
bujosa <davidbujosa@gmail.com>
2021-04-06 20:08:41 -0400

    Added order cancelled listener

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,23 @@
+import {
+  Listener,
+  OrderCancelledEvent,
+  Subjects,
+} from "@ticketing-bujosa/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ order: undefined });
+    await ticket.save();
+  }
+}
