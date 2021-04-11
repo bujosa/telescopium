bfed4cce9e5aa791f2d1b85599ad2c40fd770486
bujosa <davidbujosa@gmail.com>
2021-04-10 23:53:19 -0400

    Fixed index

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,16 +1,6 @@
-import mongoose from "mongoose";
-import { app } from "./app";
-import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
-import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
-  if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URI must be defined");
-  }
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
@@ -35,22 +25,7 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-
-    console.log("Conneting to de database");
   } catch (error) {}
-
-  app.listen(3000, () => {
-    console.log(`Listening on port 3000`);
-  });
 };
 
 start();
