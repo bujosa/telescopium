fb8b799c976b2dba7bf7b60e4138a9ecf9f2bc07
bujosa <davidbujosa@gmail.com>
2021-04-16 01:59:58 -0400

    Added ordeer cancelled publisher

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -1,9 +1,12 @@
 import {
   ExpirationCompleteEvent,
   Listener,
+  OrderStatus,
   Subjects,
 } from "@ticketing-bujosa/common";
 import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 import { queueGroupName } from "./queue-group-name";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
@@ -11,5 +14,24 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
 
-  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {}
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.order).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+      version: order.version,
+    });
+  }
 }
