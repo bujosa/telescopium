baebddd60b45b405b6c80d852ef9aae184969f9e
bujosa <davidbujosa@gmail.com>
2021-04-16 01:12:13 -0400

    Added logic on order created listener

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,21 +5,10 @@ import {
   Subjects,
 } from "@ticketing-bujosa/common";
 import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
-  queueGroupName: string;
-  onMessage(
-    data: {
-      id: string;
-      version: number;
-      status: OrderStatus;
-      user: string;
-      expiresAt: string;
-      ticket: { id: string; price: number };
-    },
-    msg: Message
-  ): void {
-    throw new Error("Method not implemented.");
-  }
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
 }
