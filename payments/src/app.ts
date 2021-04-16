2543e80317130a268c1f0d859fa79846a5fb08c3
bujosa <davidbujosa@gmail.com>
2021-04-16 03:02:11 -0400

    Added src

---
+++ payments/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  currentUser,
+  errorHandler,
+  NotFoundError,
+} from "@ticketing-bujosa/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { getAllTicketsRouter } from "./routes/tickets";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(getAllTicketsRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
