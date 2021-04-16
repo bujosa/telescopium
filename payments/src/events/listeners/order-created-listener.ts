e64baee16fd811660b7e63828ab5c2e0ee5b4bc3
bujosa <davidbujosa@gmail.com>
2021-04-16 03:29:02 -0400

    Finished order created listener

--- payments/src/events/listeners/order-created-listener.ts
+++ payments/src/events/listeners/order-created-listener.ts
@@ -4,11 +4,24 @@ import {
   Subjects,
 } from "@ticketing-bujosa/common";
 import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      user: data.user,
+      version: data.version,
+    });
+
+    await order.save();
+
+    msg.ack();
+  }
 }
