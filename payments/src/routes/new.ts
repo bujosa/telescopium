09b6ff37eb461670b19df46193e74770c9b01b98
bujosa <davidbujosa@gmail.com>
2021-04-19 04:30:05 -0400

    Added payment created listener

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -8,8 +8,10 @@ import {
 } from "@ticketing-bujosa/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
 import { Order } from "../models/order";
 import { Payment } from "../models/payment";
+import { natsWrapper } from "../nats-wrapper";
 import { stripe } from "../stripe";
 
 const router = express.Router();
@@ -47,6 +49,12 @@ router.post(
     });
 
     await payment.save();
+
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      order: payment.order,
+      stripe: payment.stripe,
+    });
 
     res.status(201).send({ success: true });
   }
