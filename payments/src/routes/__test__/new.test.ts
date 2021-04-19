fddec78a86c3216f0aa939297fb1e9734e572460
bujosa <davidbujosa@gmail.com>
2021-04-19 02:53:38 -0400

    Finished new test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -30,4 +30,22 @@ it("returns a 401 when purchasing an order that doesnt belong to the user", asyn
     .expect(401);
 });
 
-it("returns a 400 when purchasing a cancelled order", async () => {});
+it("returns a 400 when purchasing a cancelled order", async () => {
+  const user = mongoose.Types.ObjectId().toHexString();
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    user,
+    version: 0,
+    price: 30,
+    status: OrderStatus.Cancelled,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin(user))
+    .send({ token: "test", order: order.id })
+    .expect(400);
+});
