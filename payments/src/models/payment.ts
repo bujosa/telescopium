a7ef2c07db9706e5eef2aa3685bcdd30397ad87f
bujosa <davidbujosa@gmail.com>
2021-04-19 04:17:47 -0400

    Finished payment model

--- payments/src/models/payment.ts
+++ payments/src/models/payment.ts
@@ -1,1 +1,38 @@
 import mongoose from "mongoose";
+import {
+  Payment,
+  PaymentDoc,
+  PaymentModel,
+} from "../interfaces/payment.interface";
+
+const paymentSchema = new mongoose.Schema(
+  {
+    order: {
+      required: true,
+      type: String,
+    },
+    stripe: {
+      required: true,
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+paymentSchema.statics.build = (payment: Payment) => {
+  return new Payment(payment);
+};
+
+const Payment = mongoose.model<PaymentDoc, PaymentModel>(
+  "Payment",
+  paymentSchema
+);
+
+export { Payment };
