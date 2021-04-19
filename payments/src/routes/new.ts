e7dd0dc0ac31734a07dcd1c4eeaa5b2cb2f6b531
bujosa <davidbujosa@gmail.com>
2021-04-19 02:39:42 -0400

    Added test

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
-import { requireAuth } from "@ticketing-bujosa/common";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@ticketing-bujosa/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -8,7 +16,24 @@ router.post(
   "/api/payments",
   requireAuth,
   [body("token").not().isEmpty(), body("order").not().isEmpty()],
+  validateRequest,
   async (req: Request, res: Response) => {
+    const { token, order } = req.body;
+
+    const order_db = await Order.findById(order);
+
+    if (!order_db) {
+      throw new NotFoundError();
+    }
+
+    if (order_db.user !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order_db.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for an cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
